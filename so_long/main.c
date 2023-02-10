62b717d8ebac3bffe61f92cb2ff63615658a862c
Gianlu <gervasio7889@gmail.com>
2023-02-10 17:23:58 +0100

    maleducata

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: dcarassi <dcarassi@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 10:40:24 by dcarassi          #+#    #+#             */
-/*   Updated: 2023/02/10 14:59:31 by dcarassi         ###   ########.fr       */
+/*   Updated: 2023/02/10 17:23:32 by dcarassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	print_mat(char **mat)
 	i = 0;
 	while (mat[i] != NULL)
 	{
-		printf("%d -> %s\n", i, mat[i]);
+		ft_printf("%d -> %s\n", i, mat[i]);
 		i++;
 	}
 }
@@ -32,6 +32,9 @@ int main (int argc, char **argv)
 	void	*img;
 	void	*mlx_win;
 	int	dim;
+	int	i;
+	int	j;
+	int	map_linez;
 
 	if (argc != 2)
 		return(1 * ft_printf("Need a .ber file to start\n"));
@@ -43,9 +46,31 @@ int main (int argc, char **argv)
 		return (1);
 	print_mat(game.map);
 	dim = 64;
-	game.win = mlx_new_window(game.mlx, 444, 444, "!");
-	img = mlx_xpm_file_to_image(game.mlx, "Spritez/viola-1.png.xpm", &dim, &dim);
-	mlx_put_image_to_window(game.mlx, game.win, img, 0, 0);
+	// game.win = mlx_new_window(game.mlx, 444, 444, "!");
+	// mlx_put_image_to_window(game.mlx, game.win, img, 0, 0);
+	i = 0;
+	map_linez = 0;
+	while (game.map[i] != NULL)
+	{
+		j = 0;
+		while (j < ft_strlen(game.map[0]))
+		{
+			if (game.map[i][j] == '\n')
+				map_linez++;
+			j++;
+		}
+		i++;
+	}
+	// game.win = mlx_new_window(game.mlx, ft_strlen(game.map[0]), map_linez, "so_long");
+	// img = mlx_xpm_file_to_image(game.mlx, "Spritez/viola-1.png.xpm", &dim, &dim);
+	i = 0;
+	ft_printf("%d\n", map_linez);
+	ft_printf("%d\n", ft_strlen(game.map[0]) - 1);
+	game.win = mlx_new_window(game.mlx, ((ft_strlen(game.map[0]) - 1) * 64), (map_linez * 64), "so_long");
+	// while (game.map[i] != NULL)
+	// {
+
+	// }
 	mlx_loop(game.mlx);
 	}
 	return (0);
