cd5574580c1ec7d6eeecaf30271ccfedc2f2a0e2
Darkb0shy <dcarassi@localhost.localdomain>
2023-03-03 19:24:22 +0100

    test_push_swap

--- test_lis/longest_is.c
+++ test_lis/longest_is.c
@@ -63,10 +63,12 @@ int get_n_rotations(t_stack *stack_a)
   return (n_rotations);
 }
 
-void get_min_on_top_of_a_ez_mode(t_stack *stack_a)
+int get_min_on_top_of_a_ez_mode(t_stack *stack_a)
 {
   int min_index;
+  int flag;
 
+  flag = 0;
   min_index = 0;
   min_index = get_min_index_from_a(stack_a);
   if (min_index == 1)
@@ -75,12 +77,18 @@ void get_min_on_top_of_a_ez_mode(t_stack *stack_a)
     pb(stack_a, 1);
   }
   if (min_index == 0)
-    pb(stack_a, 1);
+  {
+    if (stack_a->curr_a == 0)
+      flag = 1;
+    else
+      pb(stack_a, 1);
+  }
   if (min_index == (stack_a->curr_a) - 1)
   {
     rra(stack_a, 1);
     pb(stack_a, 1);
   }
+  return (flag);
 }
 
 void  get_min_on_top_of_a_hardon_mode(t_stack *stack_a)
@@ -112,14 +120,20 @@ void  get_min_on_top_of_a_hardon_mode(t_stack *stack_a)
 void longest_is(t_stack *stacks)
 {
   int min_index;
+  int temp;
 
+  temp = 0;
   print_arraya(stacks, stacks->curr_a);
   min_index = 0;
   while (stacks->curr_a > 0)
   {
     min_index = get_min_index_from_a(stacks);
     if (min_index == 0 || min_index == 1 || min_index == (stacks->curr_a) - 1)
-      get_min_on_top_of_a_ez_mode(stacks);
+    {
+      temp = get_min_on_top_of_a_ez_mode(stacks);
+      if (temp == 1)
+        break;
+    }    
     else
     {
       get_min_on_top_of_a_hardon_mode(stacks);
